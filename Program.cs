b770ffe1939ede1de30a7f0ed496ae73c561a283
Kevin Evans <kwevans@gmail.com>
2021-06-13 00:09:07 +0100

    init
    Convert an image to a base64 string

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace dotBaseImage
+{
+    class Program
+    {
+    static public string ConvertImageToString(string filepath)
+    {
+      byte[] imageArray = System.IO.File.ReadAllBytes(filepath);
+      string base64ImageRepresentation = System.Convert.ToBase64String(imageArray);
+
+      return (base64ImageRepresentation);
+
+    }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine(ConvertImageToString("cat.jpg"));
+        }
+
+
+    }
+}
